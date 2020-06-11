d029b78f5007814931b379a8f927d6a085455d45
Bryan Rathos <brathos@wpi.edu>
2020-06-11 13:51:32 -0400

    Implemented RingBuffer class

--- src/dataStructures.h
+++ src/dataStructures.h
@@ -5,13 +5,47 @@
 #include <opencv2/core.hpp>
 
 
-struct DataFrame { // represents the available sensor information at the same time instance
+struct DataFrame 
+{ // represents the available sensor information at the same time instance
     
     cv::Mat cameraImg; // camera image
     
     std::vector<cv::KeyPoint> keypoints; // 2D keypoints within camera image
     cv::Mat descriptors; // keypoint descriptors
     std::vector<cv::DMatch> kptMatches; // keypoint matches between previous and current frame
+};
+
+template<typename FrameT>
+class RingBuffer 
+{
+private:
+    // Circular Queue
+    uint bufSize;
+    std::vector<FrameT> rBuff;
+
+public:
+    RingBuffer(uint sizeIn){
+        bufSize = sizeIn;
+    }
+
+    void push_back(FrameT frame)
+    { 
+        if(rBuff.size()+1 <= bufSize){
+            rBuff.push_back(frame);
+        }
+        else{
+            rBuff.erase(rBuff.begin());
+            rBuff.push_back(frame);
+        }
+    }
+
+    typename std::vector<FrameT>::iterator end(){
+        return rBuff.end();
+    }
+
+    uint size(){
+        return rBuff.size();
+    } 
 };
 
 
